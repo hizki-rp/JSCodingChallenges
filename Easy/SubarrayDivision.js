041a34896bcf06f6827d0360b4b7d9456919d24c
Hizkiyas Bogale Gebregiorgis <103578496+hizki-rp@users.noreply.github.com>
2023-11-05 10:23:22 +0300

    commit for when m is greater than 3

--- Easy/SubarrayDivision.js
+++ Easy/SubarrayDivision.js
@@ -1,28 +1,18 @@
 function birthday(s, d, m) {
     // Write your code here
+    let result = 0;
     let matchCount = 0;
+    
     for(let i=0;i<s.length;i++){
-        if(m===1 && s[0]===d){
+       for(let j=i;j<m;j++){
+           result += s[j];
+       }
+       if(result === d){
            matchCount += 1;
-           return matchCount;
-        }
-        for(let j=i+1;j<s.length;j++){
-            
-           if(m===2 && s[i]+s[j] === d){
-               matchCount += 1;
-           }
-           if(m===3){
-              for(let k=j+2;k<s.length;k++){
-                  if(s[i]+s[j]+s[k] === d){
-                      matchCount +=1;
-                  }
-                  return matchCount;
-              }
-           }
-           
-        }
-        return matchCount;
+       }
+       result = 0;
     }
     
+    return matchCount;
    
 }
