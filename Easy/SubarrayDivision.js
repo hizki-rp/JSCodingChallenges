0b91b40af5e119c760f5bf5d11769d335fe6d517
Hizkiyas Bogale Gebregiorgis <103578496+hizki-rp@users.noreply.github.com>
2023-11-05 14:36:11 +0300

    all test cases passed

--- Easy/SubarrayDivision.js
+++ Easy/SubarrayDivision.js
@@ -2,17 +2,15 @@ function birthday(s, d, m) {
     // Write your code here
     let result = 0;
     let matchCount = 0;
-    
+
     for(let i=0;i<s.length;i++){
-       for(let j=i;j<m;j++){
-           result += s[j];
+       for(let j=i;j<i+m;j++){
+           result += s[j]; 
        }
        if(result === d){
            matchCount += 1;
        }
        result = 0;
     }
-    
-    return matchCount;
-   
+    return matchCount;  
 }
